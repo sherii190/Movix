c5dead15cc842517b5ff63c70258bf72f9dfe62d
sherii190 <phonetech195@gmail.com>
2023-08-22 23:24:33 +0200

    store connection

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
+import {store} from "./store/store.js"
+import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
