8c643b920b404090176fbb89fbbdc1a68797e8c4
sherii190 <phonetech195@gmail.com>
2023-08-22 22:46:48 +0200

    fecth, async

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,5 +5,18 @@ const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
     Authorization: "bearer" + TMDB_TOKEN,
-    
+
+};
+
+export const fetchDataFromApi = async (url, params) =>{
+    try {
+        const {data} = await axios.get (BASE_URL + url, {
+            headers,
+            params
+        })
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
 }
