7e4918980218c649d4c8f81117c32e4dfdb74e05
sherii190 <phonetech195@gmail.com>
2023-08-22 23:58:34 +0200

    api call homeslice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from 'react'
 import {fetchDataFromApi} from "./utils/api"
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from "./store/homeSlice"
 
 
 function App() {
+  const dispatch = useDispatch()
+    
   useEffect(() => {
     apiTesting();
   }, []);
@@ -10,6 +14,7 @@ function App() {
     const apiTesting = () => {
         fetchDataFromApi("/discover/movie").then((res) => {
           console.log(res);
+          dispatch(getApiConfiguration(res))
         });
     };
 
