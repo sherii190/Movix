800933a31781ee207687a93336da591580450d1a
sherii190 <phonetech195@gmail.com>
2023-08-22 23:38:28 +0200

    store home value

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import homeSlice from "./homeSlics"
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    home: homeSlice
+  },
 })
