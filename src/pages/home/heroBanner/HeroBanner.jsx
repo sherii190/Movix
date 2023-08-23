33648c5fb97a46bb0b25daec62142e255da8a2d0
sherii190 <phonetech195@gmail.com>
2023-08-23 15:56:32 +0200

    search bar

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./style.scss"
 
 const HeroBanner = () => {
+  const[background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+
+
+const searchQueryHandler = (event) => {
+if(event.key === "Enter" && query.length > 0) {
+
+}
+
+}
+
   return (
   <div className="HeroBanner">
     <div className='wrapper'>
@@ -9,7 +20,7 @@ const HeroBanner = () => {
         <span className="title">Welcome</span>
         <span className="subTitle">Millions of movies, TV shows and people to discover. Explore now.</span>
         <div className="searchInput">
-          <input type="text" placeholder='Search for a movie or TV Show....' />
+          <input type="text" placeholder='Search for a movie or TV Show....' onChange={(e)=> setQuery(e.target.value)} oneKeyUp= {searchQueryHandler}/>
           <button>Search</button>
         </div>
       </div>
