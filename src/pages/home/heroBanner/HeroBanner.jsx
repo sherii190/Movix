8633db21a9bddfa3aa2966a2dad21230bdce3162
sherii190 <phonetech195@gmail.com>
2023-08-23 16:13:04 +0200

    search path

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,15 +1,17 @@
 import React, {useState} from 'react'
 import "./style.scss"
+import { useNavigate } from "react-router-dom";
 
 const HeroBanner = () => {
   const[background, setBackground] = useState("");
   const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
 
 const searchQueryHandler = (event) => {
-if(event.key === "Enter" && query.length > 0) {
-
-}
+        if (event.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`);
+        }
 
 }
 
@@ -20,7 +22,7 @@ if(event.key === "Enter" && query.length > 0) {
         <span className="title">Welcome</span>
         <span className="subTitle">Millions of movies, TV shows and people to discover. Explore now.</span>
         <div className="searchInput">
-          <input type="text" placeholder='Search for a movie or TV Show....' onChange={(e)=> setQuery(e.target.value)} oneKeyUp= {searchQueryHandler}/>
+          <input type="text" placeholder='Search for a movie or TV Show....' onChange={(e) => setQuery(e.target.value)} onKeyUp={searchQueryHandler}/>
           <button>Search</button>
         </div>
       </div>
